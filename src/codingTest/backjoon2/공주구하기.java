5bdc9ac409ec3eff5c81fde07d33b81eee1475e5
said1573 <therapy23@naver.com>
2022-04-01 17:18:13 +0900

    2022.04.01 공주 구하기

---
+++ src/codingTest/backjoon2/공주구하기.java
@@ -0,0 +1,33 @@
+package codingTest.backjoon2;
+
+import codingTest.test.A;
+
+import java.util.*;
+
+public class 공주구하기 {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        int people = scanner.nextInt();
+        int tempo = scanner.nextInt();
+
+        Queue<Integer> queue = new LinkedList<>();
+        for (int i = 1; i <= people; i++) {
+            queue.offer(i);
+        }
+
+        int count = 0;
+        while (queue.size() > 1) {
+            count++;
+            int poll = queue.poll();
+            if (count == tempo) {
+                count = 0;
+            } else {
+                queue.offer(poll);
+            }
+        }
+
+        System.out.println(queue.peek());
+
+    }
+}
